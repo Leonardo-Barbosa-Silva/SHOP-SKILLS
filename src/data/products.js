a780633eaa37fad1724f39b773729b639804301c
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-09-01 09:36:25 -0300

    adjusting image path

--- src/data/products.js
+++ src/data/products.js
@@ -4,27 +4,27 @@ export const shopProducts = [
         title: "Comunication",
         price: 500.00,
         description: "Speak fluently in English in 2023.",
-        image: ".src/images/tec.jpeg"
+        image: "./images/tec.jpeg"
     },
     {
         id: 10002,
         title: "Programming",
         price: 700.00,
         description: "Became a better programmer in 2023.",
-        image: ".src/images/tec.jpeg"
+        image: "./images/tec.jpeg"
     },
     {
         id: 10003,
         title: "Persuasion",
         price: 1000.00,
         description: "Magnetic person in 2023.",
-        image: ".src/images/tec.jpeg"
+        image: "./images/tec.jpeg"
     },
     {
         id: 10004,
         title: "Emotional Intelligence",
         price: 1200.00,
         description: "Magnific emotional intelligence in 2023.",
-        image: ".src/images/tec.jpeg"
+        image: "./images/tec.jpeg"
     }
 ];
